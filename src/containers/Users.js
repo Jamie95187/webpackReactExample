5571806c1514ccd814bd380861b824e0eda44510
Jamie95187 <jamiewong63@googlemail.com>
2020-05-01 10:05:18 +0100

    setting basic webpack conifg file

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React { Component } from 'react';
+
+import PizzaImage from '../components/PizzaImage/PizzaImage';
+
+class Pizza extends Component {
+  render () {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Henlo Frendz I Love pizza</p>
+      </div>
+    );
+  }
+}
+
+export default User;
